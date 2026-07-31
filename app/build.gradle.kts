059eedc73e83d49314857dbd044a09624b54bb32
xf8410 <1421506586@qq.com>
2026-07-31 15:00:58 +0800

    chore: bump Workbench version to 1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.newoether.agora.workbench"
         minSdk = 24
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.4.2-workbench"
+        versionCode = 31
+        versionName = "1.4.3-workbench"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
